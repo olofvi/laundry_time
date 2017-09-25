c19c268f4fff590b14658fd24c1bd5e52a1f89ee
Nurlan Emir <nurlan.emiroff@gmail.com>
2017-09-25 16:17:44 +0200

    Setting up Travis and Coverall (#5)
    * added travis configuration
    
    * rspec init
    
    * coveralls set up
    
    * removed rspec gem
    
    * travis and coveralls set up

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,3 +1,5 @@
+require 'coveralls'
+Coveralls.wear_merged!('rails')
 require 'cucumber/rails'
 ActionController::Base.allow_rescue = false
 
@@ -7,4 +9,3 @@ rescue NameError
   raise "You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it."
 end
 Cucumber::Rails::Database.javascript_strategy = :truncation
-
