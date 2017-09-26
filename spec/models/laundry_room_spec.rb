60ad0f80a4d97ce510fda8bce520ba01bbc4d79c
Amanda <29342045+Sajaas@users.noreply.github.com>
2017-09-26 13:38:36 +0200

    updated test, still failing

--- spec/models/laundry_room_spec.rb
+++ spec/models/laundry_room_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 RSpec.describe LaundryRoom, type: :model do
 
   before do
-    @laundry_room = LaundryRoom.new
+    @laundry_room = LaundryRoom.create
   end
 
   it 'has laundry room attributes' do
-    expect(@laundry_room).to have_attributes(time_type: fixed, capacity_type: closed)
+    expect(@laundry_room).to have_attributes(time_type: :fixed, capacity_type: :closed)
   end
 end
