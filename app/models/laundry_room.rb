e1aff49a94adeecb7039d5ac48c2715ae588642f
Amanda <29342045+Sajaas@users.noreply.github.com>
2017-09-26 11:30:12 +0200

    added laundryRoom time_type fixed, capacity_type closed

--- app/models/laundry_room.rb
+++ app/models/laundry_room.rb
@@ -1,4 +1,6 @@
 class LaundryRoom < ApplicationRecord
-  acts_as_bookable time_type: :fixed
-  schedule: :text
+  acts_as_bookable  time_type: :fixed,
+                    schedule: :text,
+                    capacity_type: :closed,
+                    capacity: :integer
 end
