0653d5e74a88eba693732783886bafab000fa1c1
olofvi <olofvi89@gmail.com>
2017-09-26 13:38:39 +0200

    tried to make a schedule

--- app/models/laundry_room.rb
+++ app/models/laundry_room.rb
@@ -2,3 +2,9 @@ class LaundryRoom < ApplicationRecord
   acts_as_bookable  time_type: :fixed,
                     capacity_type: :closed
 end
+
+@laundry_room = LaundryRoom.new
+@laundry_room.schedule = IceCube::Schedule.new
+# This show is available every day at 6PM and 10PM
+@laundry_room.schedule.add_recurrence_rule IceCube::Rule.daily.hour_of_day(09,15)
+@laundry_room.save!
