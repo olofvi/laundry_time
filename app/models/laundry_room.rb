82ce5e7d39076cd53934d410ea9181beb9f48fed
Amanda <29342045+Sajaas@users.noreply.github.com>
2017-09-26 11:48:10 +0200

    removed schedule attribute

--- app/models/laundry_room.rb
+++ app/models/laundry_room.rb
@@ -1,6 +1,5 @@
 class LaundryRoom < ApplicationRecord
   acts_as_bookable  time_type: :fixed,
-                    schedule: :text,
                     capacity_type: :closed,
                     capacity: :integer
 end
