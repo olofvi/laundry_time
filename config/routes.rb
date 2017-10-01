bb1a9318cf0a18161ac7b167d60524d9f35fb616
olofvi <olofvi89@gmail.com>
2017-10-01 12:14:05 +0200

    the calendar works in the server

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'calendar/index'
+
   root controller: :laundry_rooms, action: :index
   resources :laundry_rooms, only: [:index] do
     get :create_booking
